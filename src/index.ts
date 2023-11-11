a9a5ec7d14faf0911f86eec793caa00e1a2ab914
PaarthIyer314 <pi@DESKTOP-1CKS5FO>
2023-11-11 12:25:31 +0530

    feat: changed code to use the cmi payment portal

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,22 @@ const api_url = env.get('DIRECTUS_API_URL').required().asUrlString()
 const api_token = env.get('DIRECTUS_API_TOKEN').required().asString()
 
 const merchantId = env.get('AIRPAY_MERCHANT_ID').required().asString()
-const private_key = env.get('AIRPAY_PRIVATEKEY').required().asString()
-const keydata = env.get('AIRPAY_KEYDATA').required().asString()
+// const private_key = env.get('AIRPAY_PRIVATEKEY').required().asString()
+// const keydata = env.get('AIRPAY_KEYDATA').required().asString()
 const username = env.get('AIRPAY_USERNAME').required().asString()
 
 const app = new Elysia()
     .use(html())
     .get('/', () => 'OK')
     .get('/send/:orderid', ({ params: { orderid } }: { params: { orderid: string } }) =>
-        pay(api_url, api_token, orderid, merchantId, private_key, keydata)
+        pay(
+            api_url,
+            api_token,
+            orderid
+            // merchantId,
+            // private_key,
+            // keydata
+        )
     )
     .post('/receive', ({ body }) => receive(api_url, api_token, merchantId, username, body))
     .listen({ hostname: host, port: port })
--- src/pay.tsx
+++ src/pay.tsx
@@ -1,43 +1,43 @@
 import { createDirectus, rest, staticToken, readItem } from '@directus/sdk'
-import dateFormat from 'dateformat'
+// import dateFormat from 'dateformat'
 
 export async function pay(
     api_url: string,
     api_token: string,
-    orderid: string,
-    merchantId: string,
-    private_key: string,
-    keydata: string
+    orderid: string
+    // merchantId: string,
+    // private_key: string,
+    // keydata: string
 ) {
     const client = createDirectus(api_url).with(staticToken(api_token)).with(rest())
 
     const result = await client.request(readItem('payment', orderid))
     console.log(result)
 
-    let sha256 = require('sha256')
+    // let sha256 = require('sha256')
 
     const buyerEmail = result.buyerEmail
     const buyerPhone = result.buyerPhone
     const buyerFirstName = result.buyerFirstName
     const buyerLastName = result.buyerLastName
     const order_id = result.orderid
-    const amount = result.amount + '.00' //amount should always be integer when read from directus
-    const uid = result.uid
-    const currency = 356
-    const isocurrency = 'INR'
+    const amount = result.amount //amount should always be integer when read from directus
+    // const uid = result.uid
+    // const currency = 356
+    // const isocurrency = 'INR'
 
-    const now = new Date()
-    const alldata =
-        buyerEmail +
-        buyerFirstName +
-        buyerLastName +
-        amount +
-        order_id +
-        uid +
-        dateFormat(now, 'yyyy-mm-dd')
-    console.log({ alldata })
-    const checksum = sha256(keydata + '@' + alldata)
-    console.log({ checksum })
+    // const now = new Date()
+    // const alldata =
+    //     buyerEmail +
+    //     buyerFirstName +
+    //     buyerLastName +
+    //     amount +
+    //     order_id +
+    //     uid +
+    //     dateFormat(now, 'yyyy-mm-dd')
+    // console.log({ alldata })
+    // const checksum = sha256(keydata + '@' + alldata)
+    // console.log({ checksum })
 
     return (
         <html lang='en'>
@@ -51,27 +51,25 @@ export async function pay(
             <body>
                 <div>
                     Do Not Refresh or Press Back <br />
-                    Redirecting to Airpay
+                    Redirecting to CMI Payment Gateway
                 </div>
                 <form
                     id='sendtoairpay'
-                    method='POST'
-                    action='https://payments.airpay.co.in/pay/index.php'
+                    method='GET'
+                    action='https://www.schoolpay.co.in/form_generator/view.php'
                 >
-                    <input type='hidden' name='buyerEmail' value={buyerEmail} />
-                    <input type='hidden' name='buyerPhone' value={buyerPhone} />
-                    <input type='hidden' name='buyerFirstName' value={buyerFirstName} />
-                    <input type='hidden' name='buyerLastName' value={buyerLastName} />
-                    <input type='hidden' name='orderid' value={order_id} />
-                    <input type='hidden' name='amount' value={amount.toString()} />
-                    <input type='hidden' name='uid' value={uid} />
-                    <input type='hidden' name='privatekey' value={private_key} />
-                    <input type='hidden' name='mercid' value={merchantId} />
-                    <input type='hidden' name='kittype' value='server_side_sdk' />
-                    <input type='hidden' name='checksum' value={checksum} />
-                    <input type='hidden' name='currency' value={currency.toString()} />
-                    <input type='hidden' name='isocurrency' value={isocurrency} />
-                    <input type='hidden' name='customvar' value='STEMSregistration' />
+                    <input type='hidden' name='id' value='81393' />
+                    <input type='hidden' name='element_306' value='23' />
+                    <input
+                        type='hidden'
+                        name='element_405'
+                        value={buyerFirstName + ' ' + buyerLastName}
+                    />
+                    <input type='hidden' name='element_100' value={buyerPhone} />
+                    <input type='hidden' name='element_408' value={buyerEmail} />
+                    <input type='hidden' name='element_327' value={order_id} />
+                    <input type='hidden' name='element_409_1' value={amount} />
+                    <input type='hidden' name='element_409_2' value='00' />
                 </form>
             </body>
         </html>
